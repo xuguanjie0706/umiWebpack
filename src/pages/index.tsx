0155949a8e960f58212568b4626bf5bc8ee00870
xuguanjie <xuguanjie@halove.com>
2020-09-10 14:18:11 +0800

    bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import { Link } from 'umi';
 export default () => {
   return (
     <div>
-      <h1 className={styles.title}>Page index eicp</h1>
+      <h1 className={styles.title}>Page indexfdsdf eicp</h1>
       <Link to="./app1">去菜单1</Link>
     </div>
   );
